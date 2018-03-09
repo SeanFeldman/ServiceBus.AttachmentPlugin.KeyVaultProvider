2a57cd93ab09dcc85fd75a958c6864fdd5668529
Sean Feldman <feldman.sean@gmail.com>
2018-03-09 10:56:14 -0700

    Add initial documentation and base code

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ServiceBus.AttachmentPlugin.KeyVaultProvider
-KeyVault provider for ServiceBus.AttachmentPlugin
+![Icon](https://github.com/SeanFeldman/ServiceBus.AttachmentPlugin.KeyVaultProvider/blob/master/images/project-icon.png)
+
+### This is a KeyVault provider for  [ServiceBus.AttachmentPlugin add-in](https://github.com/SeanFeldman/ServiceBus.AttachmentPlugin)
+
+License: [![License](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/SeanFeldman/ServiceBus.AttachmentPlugin.KeyVaultProvider/blob/master/LICENSE)
+
+### Nuget package
+
+[![NuGet Status](https://buildstats.info/nuget/ServiceBus.AttachmentPlugin?includePreReleases=true)](https://www.nuget.org/packages/ServiceBus.AttachmentPlugin.KeyVaultProvider/)
+
+Available here http://nuget.org/packages/ServiceBus.AttachmentPlugin.KeyVaultProvider
+
+To Install from the Nuget Package Manager Console 
+    
+    PM> Install-Package ServiceBus.AttachmentPlugin.KeyVaultProvider
+
+## Examples
+
+### Convert body into attachment, no matter how big it is
+
+Configuration and registration
+
+```c#
+var provider = new KeyVaultProvider("client-id", "client-secret", "secret-identifier");
+var config = new AzureStorageAttachmentConfiguration(provider);
+var sender = new MessageSender(...);
+sender.RegisterAzureStorageAttachmentPlugin(config);
+```
+
+## Icon
+
+Created by Gregor Cresnar from the Noun Project
---
+++ src/KeyVaultProvider/KeyVaultProvider/KeyVaultProvider.cs
@@ -0,0 +1,72 @@
+﻿namespace ServiceBus.AttachmentPlugin
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class KeyVaultProvider : IProvideStorageConnectionString
+    {
+        private readonly string clientId;
+        private readonly string clientSecret;
+        private readonly string secretIdentifier;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <param name="clientSecret"></param>
+        /// <param name="secretIdentifier"></param>
+        public KeyVaultProvider(string clientId, string clientSecret, string secretIdentifier)
+        {
+            Guard.AgainstEmpty(nameof(clientId), clientId);
+            Guard.AgainstEmpty(nameof(clientSecret), clientSecret);
+            Guard.AgainstEmpty(nameof(secretIdentifier), secretIdentifier);
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+            this.secretIdentifier = secretIdentifier;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetConnectionString()
+        {
+            var client = new KeyVaultClient(GetToken);
+
+            // possible KeyVaultErrorException : https://docs.microsoft.com/en-us/azure/key-vault/key-vault-dotnet2api-release-notes#exceptions
+            // message is always in exception.Body.Error.Message && .Code (for a string code)
+            var secretBundle = await client.GetSecretAsync(secretIdentifier)
+                .ConfigureAwait(false);
+
+            return secretBundle.Value;
+        }
+
+        async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+
+            var clientCredential = new ClientCredential(clientId, clientSecret);
+
+            var authenticationResult = await authContext.AcquireTokenAsync(resource, clientCredential)
+                .ConfigureAwait(false);
+
+            if (authenticationResult == null)
+            {
+                throw new InvalidOperationException("Failed to retrieve access token for Key Vault");
+            }
+
+            return authenticationResult.AccessToken;
+        }
+    }
+
+    // TODO: remove when plugin has this contract
+    internal interface IProvideStorageConnectionString
+    {
+        Task<string> GetConnectionString();
+    }
+}
